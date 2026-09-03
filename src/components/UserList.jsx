aa4f0c737955c3ab35d7dcbffb919b99cc82da0f
abhay-a-nayak2305 <abhayanayakSHHP@gmail.com>
2026-09-03 23:24:49 +0530

    Fix module resolution: add .js extension to useFetch import

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useFetch } from './hooks/useFetch';
+import { useFetch } from './hooks/useFetch.js';
 import './UserList.css';
 
 const URL = 'https://jsonplaceholder.typicode.com/users';
